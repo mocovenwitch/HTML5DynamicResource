4e4a30b5cfd9353bae1b66ae639637aa30df2abb
Mocoven <mocovenwitch@gmail.com>
2013-07-18 00:29:22 +0800

    urgent, didn't do the ignore file

---
+++ gen/com/moco/html5dynamicresource/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.moco.html5dynamicresource;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/moco/html5dynamicresource/MainActivity.java
@@ -0,0 +1,39 @@
+package com.moco.html5dynamicresource;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+	WebView webview = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		webview = (WebView)findViewById(R.id.web);
+		webview.loadUrl("file:///android_asset/www/index.html");
+		
+		webview.setWebViewClient(new WebViewClient() {
+			@Override
+		    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+		       view.loadUrl("file:///android_asset/www/new-theme.css");
+		       return true;
+		    }
+		});
+		
+	}
+	
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
