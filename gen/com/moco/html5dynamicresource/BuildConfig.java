203f120d5f2e6cd1bd7ff644e9966286056c4da9
Mocoven <mocovenwitch@gmail.com>
2013-07-23 00:08:15 +0800

    remove unnecessary code

--- gen/com/moco/html5dynamicresource/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.moco.html5dynamicresource;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/moco/html5dynamicresource/MainActivity.java
+++ src/com/moco/html5dynamicresource/MainActivity.java
@@ -1,13 +1,10 @@
 package com.moco.html5dynamicresource;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringBufferInputStream;
-import java.util.Random;
 
 import android.os.Bundle;
 import android.os.Environment;
@@ -30,8 +27,6 @@ public class MainActivity extends Activity {
 		
 		webview = (WebView)findViewById(R.id.web);
 		webview.setWebViewClient(new MyWebView());
-//		webview.loadUrl("http://www.mocoven.com");
-		
 		webview.loadUrl("file:///android_asset/www/index.html");
 		
 		
@@ -40,7 +35,7 @@ public class MainActivity extends Activity {
 //		webview.getSettings().setJavaScriptEnabled(true);
 //		webview.getSettings().setBuiltInZoomControls(true);
 //		webview.loadUrl("file://mnt/sdcard/files/index.html");
-//		webview.loadUrl("http://www.google.com.hk");
+//		webview.loadUrl("http://www.mocoven.com");
 		
 	}
 
@@ -48,7 +43,7 @@ public class MainActivity extends Activity {
 		@SuppressLint("NewApi")
 		@Override
         public WebResourceResponse shouldInterceptRequest (final WebView view, String url) {
-        	Log.i("--shouldInterceptRequest---", "url: " + url);
+        	Log.i("shouldInterceptRequest", "url: " + url);
         	if (url.contains(".css")) {
                 return getCssWebResourceResponseFromExternalStorage();
             } else {
@@ -59,18 +54,19 @@ public class MainActivity extends Activity {
         /**
          * Return WebResourceResponse with CSS markup from a String. 
          */
-        @SuppressWarnings("deprecation")
         private WebResourceResponse getCssWebResourceResponseFromString() {
-        	Log.i("------", "getCssWebResourceResponseFromString");
-            return getUtf8EncodedCssWebResourceResponse(new StringBufferInputStream("body { background-color: #F781F3; }"));
+        	Log.i("getCssWebResourceResponseFromString", "");
+        	String style = "body { background-color: #123456; }";
+        	InputStream is = new ByteArrayInputStream(style.getBytes());
+            return getUtf8EncodedCssWebResourceResponse(is);
         }
 
         /**
-         * Return WebResourceResponse with CSS markup from an asset (e.g. "assets/style.css"). 
+         * Return WebResourceResponse with CSS markup from an asset (e.g. "assets/www/resource/new-theme.css"). 
          */
-        private WebResourceResponse getCssWebResourceResponseFromAsset() {
+		private WebResourceResponse getCssWebResourceResponseFromAsset() {
             try {
-            	Log.i("------", "getCssWebResourceResponseFromAsset");
+            	Log.i("getCssWebResourceResponseFromAsset", "");
                 return getUtf8EncodedCssWebResourceResponse(getAssets().open("www/resource/new-theme.css"));
             } catch (IOException e) {
                 return null;
@@ -82,7 +78,7 @@ public class MainActivity extends Activity {
          */
         private WebResourceResponse getCssWebResourceResponseFromExternalStorage() {
             try {
-            	Log.i("------", "getCssWebResourceResponseFromAsset");
+            	Log.i("getCssWebResourceResponseFromExternalStorage", "");
             	File file = new File(Environment.getExternalStorageDirectory().getPath() + "/files/external-theme.css");
             	FileInputStream cssfile = new FileInputStream(file);
                 return getUtf8EncodedCssWebResourceResponse(cssfile);
@@ -90,13 +86,6 @@ public class MainActivity extends Activity {
                 return null;
             }
         }
-
-        /**
-         * Return WebResourceResponse with CSS markup from a raw resource (e.g. "raw/style.css"). 
-         */
-//        private WebResourceResponse getCssWebResourceResponseFromRawResource() {
-//            return getUtf8EncodedCssWebResourceResponse(getResources().openRawResource(R.raw.style));
-//        }
 
         @SuppressLint("NewApi")
 		private WebResourceResponse getUtf8EncodedCssWebResourceResponse(InputStream data) {
@@ -107,10 +96,8 @@ public class MainActivity extends Activity {
 	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
 			Log.i("shouldOverrideUrlLoading", "url:" + url);
 			
-//			String htmlData = "<link rel=\"stylesheet\" type=\"text/css\" href=\"www/resource/new-theme.css\" />";
-//			view.loadDataWithBaseURL("file:///android_asset/", htmlData, "text/html", "UTF-8", null);
-			
-			String htmlData = "<!DOCTYPE html> <head> <meta charset='UTF-8'> <title>Load the CSS file from the sdcard</title> <link href='www/resource/new-theme.css' rel='stylesheet'> </head> <body> <div id='show_me'>origin is grey; new CSS in assets is png Mocoven Avatar on the below; external CSS in sdcard is Base64 Mocoven Avatar on the top;</div> <a href='http://www.google.com.hk'>sdfdsfdsf</a> <div id='mocoven'>png file</div> </body> </html>";
+			// the path to .css file is relative to baseurl, here is "file:///android_asset/"
+			String htmlData = "<!DOCTYPE html> <head> <meta charset='UTF-8'><title>Load the CSS file from the sdcard</title><link href='www/resource/new-theme.css' rel='stylesheet'></head><body><div id='show_me'>origin is grey; new CSS in assets is png Mocoven Avatar on the below; external CSS in sdcard is Base64 Mocoven Avatar on the top;</div> <a href='http://www.google.com.hk'>Google</a> <div id='mocoven'>png file</div> </body> </html>";
 			webview.loadDataWithBaseURL("file:///android_asset/", htmlData, "text/html", "UTF-8", null);
 	        return true;
 	    }
